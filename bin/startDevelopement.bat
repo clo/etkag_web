aa120eb669f8f5919178343dc0235febf8d212de
Lochmatter Christian <christian.lochmatter@gmx.ch>
2011-05-04 13:07:22 +0200

    added batch file to start development environment

---
+++ bin/startDevelopement.bat
@@ -0,0 +1,5 @@
+start "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost
+cd "Z:\LOC\Program Files\NetBeans 6.9.1\bin"
+start netbeans.exe
+"Z:\LOC\kjoff\bin\3rd\xampp-win32-1.7.3\xampp\xampp-control.exe"
+pause
