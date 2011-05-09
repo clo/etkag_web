9f203e3759508950bf0d1185ba862cfea2fb05a4
Lochmatter Christian <christian.lochmatter@gmx.ch>
2011-05-09 07:32:58 +0200

    test with picasa service from google

--- picasa.php
+++ picasa.php
@@ -7,32 +7,22 @@ session_start();
 include("header.php");
 set_include_path("lib;".  get_include_path());
 error_reporting(E_ALL);
-$me = $_PHP['self'];
+$me = basename($_SERVER['PHP_SELF']);
 
+/*
+ * Load Zend framework for handling picasa web albums
+ */
 require_once 'Zend/Loader.php';
 //Zend_Loader::laodClass('Zend_Gdata_Photos_UserQuery');
 //Zend_Loader::laodClass('Zend_Gdata_Photos_PhotoQuery');
 Zend_Loader::loadClass('Zend_Gdata_Photos_UserQuery');
 Zend_Loader::loadClass('Zend_Gdata_Photos_AlbumQuery');
+Zend_Loader::loadClass('Zend_Gdata_Photos_PhotoQuery');
 Zend_Loader::loadClass('Zend_Gdata_Photos');
 Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
 Zend_Loader::loadClass('Zend_Gdata_AuthSub');
 echo "Include Path: " . get_include_path()."<br />\n";
 echo "PICASA Test<br />\n";
-
-// Konfigurationsparameter setzen
-//$config = array(
-//    'adapter'    => 'Zend_Http_Client_Adapter_Proxy',
-//    'proxy_host' => 'proxy.swisscom.com',
-//    'proxy_port' => 8080,
-//    'proxy_user' => '',
-//    'proxy_pass' => ''
-//);
-//
-//// Client-Objekt instanziieren
-//$clientproxy = new Zend_Http_Client('http://www.example.com', $config);
-//
-//// $client kann jetzt wie gewohnt benutzt werden
 
 $serviceName = Zend_Gdata_Photos::AUTH_SERVICE_NAME;
 $user = $google_picasa_user;
@@ -64,7 +54,7 @@ $gp = new Zend_Gdata_Photos($client);
 //    echo "</pre>";
 //}
 
-echo "<pre>";
+//echo "<pre>";
 //$query = $gp->newAlbumQuery();
 //$query->setUser("default");
 //$query->setAlbumName("Ablagebox");
@@ -81,50 +71,76 @@ echo "<pre>";
 //foreach ($albumFeed as $albumEntry) {
 //    echo $albumEntry->title->text . "<br />\n";
 //}
+echo "<a class='main' href='$me?session=clear'>Zeige alle Albums</a><br />\n";
 print_r($_SESSION);
+if (isset($_GET['session']) && $_GET['session'] == 'clear'){
+   session_destroy();
+}
 if (isset($_GET['albumname'])){
     $_SESSION['albumname']=$_GET['albumname'];
 }
+/* -----------------------------------------------------------------------------
+ * no albumname is given, list all availalbe albums
+ * -------------------------------------------------------------------------- */
 if (!isset($_SESSION['albumname'])) {
-    $query = new Zend_Gdata_Photos_UserQuery();
+    $query = new Zend_Gdata_Photos_AlbumQuery();
     $query->setUser("christian.lochmatter");
-    $query->setType("entry");
+    //$query->setAlbumName($_SESSION['albumname']);
+    //$query->setType("entry");
 
     try {
-        //$albumFeed = $gp->getAlbumFeed($query);
-        $userFeed = $gp->getUserFeed("default");
-        $link = $userFeed->getEntry();
+        $userFeed = $gp->getUserFeed('default');
         foreach ($userFeed->getEntry() as $l){
           $title = $l->getTitle()->getText();
-          echo "<a class='main' href='$me?albumname=$title'>$title</a><br />";
-          //print_r($l->getLink());
+          echo "<a class='main' href='$me?albumname=$title'>$title</a><br />\n";
         }
-    //echo "Album: " . $albumFeed->getEntry();
-        //print_r($link);
-        //print_r($userFeed);
     } catch (Zend_Gdata_App_Exception $e) {
         echo "Error: " . $e->getMessage();
         echo "Trace: " . $e->getTrace();
     }
+/* -----------------------------------------------------------------------------
+ * albumname is given
+ * -------------------------------------------------------------------------- */
 } elseif(isset($_SESSION['albumname'])) {
-    echo $_SESSION['albumname'];
-    $query = new Zend_Gdata_Photos_AlbumQuery();
+    echo $_SESSION['albumname']."<br />\n";
+    //$query = new Zend_Gdata_Photos_AlbumQuery();
+
+    $query = $gp->newAlbumQuery();
     $query->setUser("christian.lochmatter");
-    $query->setType("entry");
-    $query->setAlbumName($_SESSION['albumname']);
+    $query->setAlbumName(str_replace(" ","",$_SESSION['albumname']));
+
     try {
         //$albumFeed = $gp->getAlbumFeed($query);
-        $userFeed = $gp->getUserFeed($query);
-        $link = $userFeed->getEntry();
-        foreach ($userFeed->getEntry() as $l){
-          $title = $l->getTitle()->getText();
-          echo "<a class='main' href='$me?albumname=$title'>$title</a><br />";
-          //print_r($l->getLink());
+        $albumFeed = $gp->getAlbumFeed($query);
+        //  echo "<pre>";
+        //print_r($ablumFeed);
+        foreach ($albumFeed as $albumEntry){
+          if ($albumEntry->getMediaGroup()->getThumbnail() != null) {
+            $url = $albumEntry->getLink('alternate')->href;
+            //echo $url . "\n";
+            $mediaThumbnailArray = $albumEntry->getMediaGroup()->getThumbnail();
+            $ThumbnailUrl = $mediaThumbnailArray[0]->getUrl();
+            $ThumbnailHeight = $mediaThumbnailArray[0]->getHeight();
+            $ThumbnailWidth = $mediaThumbnailArray[0]->getWidth();
+            // Load Picture Info
+            $mediaArray = $albumEntry->getMediaGroup()->getContent();
+            $ImageUrl = $mediaArray[0]->getUrl();
+            $sImageTitle = $albumEntry->getMediaGroup()->getDescription()->text;
+            $url = $albumEntry->getLink('alternate')->href;
+
+            //$sLinkString = <<<LTEXT
+            echo "<a href='$ImageUrl' title='$sImageTitle' rel='lightbox'><img src='$ThumbnailUrl' width='$ThumbnailWidth' height='$ThumbnailHeight' /></a><br />";
+            //LTEXT;
+            //$sPrintThumbs .=$sLinkString ;
+          }else{
+            echo "NOTHING";
+          }
         }
-    //echo "Album: " . $albumFeed->getEntry();
+        //echo "Album: " . $albumFeed->getEntry();
         //print_r($link);
         //print_r($userFeed);
     } catch (Zend_Gdata_App_Exception $e) {
+        echo "<pre>";   
         echo "Error: " . $e->getMessage();
         echo "Trace: " . $e->getTrace();
     }
