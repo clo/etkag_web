3ffe1eff90eed42ee3ffdd35ca977215745d2e28
Lochmatter Christian <christian.lochmatter@gmx.ch>
2011-12-05 17:10:28 +0100

    removed all under doc directory

--- doc/news/24122010_Weihnachtsgruss/ETK _AG_weihnachtsgruss.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-
-<iframe title="YouTube video player" class="youtube-player" type="text/html" width="599" height="362" src="http://www.youtube.com/embed/FXgavpV3GVo?rel=0" frameborder="0"></iframe>
